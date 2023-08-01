bf4a843bc17c22ae622868c74b10897dc6e9aa50
ahmadjemel <ahmad.jemel10@gmail.com>
2023-08-01 22:30:27 +0100

    Commit new version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import NavScroll from './components/NavScroll';
+import Cart from './components/articles/Cart';
+import Listarticles from './components/articles/Listarticles';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import StripePayment from './components/articles/StripePayment';
+import CheckoutSuccess from './components/articles/CheckoutSuccess';
+import PdfCart from './components/articles/PdfCart';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import ProductsAppAdmin from './admin/components/articles/ProductsAppAdmin';
+import { ToastContainer } from 'react-toastify';
+import Login from './admin/components/login';
+import Logout from './admin/components/logout';
+import Register from './admin/components/Register';
+import ProtectedRoutes from './admin/ProtectedRoute';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer />
+      <Router>
+
+
+
+        <Routes>
+        <Route element={<ProtectedRoutes/>}>
+        <Route path='/articlesadmin' element={<ProductsAppAdmin />} />
+
+        </Route>
+
+          <Route path='/' element={<Listarticles />} ></Route>
+          <Route path='/articles' element={<Listarticles />} ></Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/pay/:total' element={<StripePayment />} />
+          <Route path="/checkout" element={<CheckoutSuccess />} />
+          <Route path="/pdfcart" element={<PdfCart />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/logout' element={<Logout />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/menu' element={< NavScroll />} />
+          <Route path='*' element={<Nopage/>}/>
+
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import store from '../src/components/redux/store';
+import { Provider } from 'react-redux' ;
+import 'react-toastify/dist/ReactToastify.css';
+import {
+  persistStore,
+  } from 'redux-persist'
+  import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let persistor = persistStore(store)
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+    <Router>
+<App />
+</Router>
+</PersistGate>
+</Provider>
   </React.StrictMode>
 );
 
